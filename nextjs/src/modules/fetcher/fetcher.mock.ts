be1ad376ffa5f0ce1a12e8c71aec5da23b91b9e3
Alanderson Zelindro da Rosa <alandersonzelindrodr@gmail.com>
2024-07-26 10:59:02 -0300

    fix: update fetcher export and add delete and update

--- nextjs/src/modules/fetcher/fetcher.mock.ts
+++ nextjs/src/modules/fetcher/fetcher.mock.ts
@@ -1,16 +1,16 @@
-import FetcherClient from './fetcher.interface'
-import { IFetchConfig, IFetchResponse } from './types'
+import FetcherClient from "./fetcher.interface";
+import { IFetchConfig, IFetchResponse } from "./types";
 
 class FetcherMock extends FetcherClient {
-  private responseData: any = {}
-  private responseStatus: number = 200
+  private responseData: any = {};
+  private responseStatus: number = 200;
 
   public setResponseData(data: any) {
-    this.responseData = data
+    this.responseData = data;
   }
 
   public setResponseStatus(status: number) {
-    this.responseStatus = status
+    this.responseStatus = status;
   }
 
   public async post<T = any>(path: string, config?: IFetchConfig) {
@@ -19,12 +19,26 @@ class FetcherMock extends FetcherClient {
       headers: new Headers(),
       request: {
         url: this.getApiUrl(path),
-        ...config
+        ...config,
       } as any,
       response: {
-        status: this.responseStatus
+        status: this.responseStatus,
       } as any,
-    } as IFetchResponse<T>
+    } as IFetchResponse<T>;
+  }
+
+  public async put<T = any>(path: string, config?: IFetchConfig) {
+    return {
+      data: this.responseData,
+      headers: new Headers(),
+      request: {
+        url: this.getApiUrl(path),
+        ...config,
+      } as any,
+      response: {
+        status: this.responseStatus,
+      } as any,
+    } as IFetchResponse<T>;
   }
 
   public async get<T = any>(path: string, config?: IFetchConfig) {
@@ -33,13 +47,27 @@ class FetcherMock extends FetcherClient {
       headers: new Headers(),
       request: {
         url: this.getApiUrl(path),
-        ...config
+        ...config,
       } as any,
       response: {
-        status: this.responseStatus
+        status: this.responseStatus,
       } as any,
-    } as IFetchResponse<T>
+    } as IFetchResponse<T>;
+  }
+
+  public async delete<T = any>(path: string, config?: IFetchConfig) {
+    return {
+      data: this.responseData,
+      headers: new Headers(),
+      request: {
+        url: this.getApiUrl(path),
+        ...config,
+      } as any,
+      response: {
+        status: this.responseStatus,
+      } as any,
+    } as IFetchResponse<T>;
   }
 }
 
-export default FetcherMock
+export default FetcherMock;
--- nextjs/src/modules/fetcher/index.ts
+++ nextjs/src/modules/fetcher/index.ts
@@ -1,4 +1,4 @@
-export { default } from './fetcher'
-export { default as FetcherClient } from './fetcher.interface'
-export { default as FetcherError } from './fetcher.error'
-export type { IFetchConfig, IFetchResponse, IRequestProperties } from './types'
+export { default as Fetcher } from "./fetcher";
+export { default as FetcherClient } from "./fetcher.interface";
+export { default as FetcherError } from "./fetcher.error";
+export type { IFetchConfig, IFetchResponse, IRequestProperties } from "./types";
